800e175db67d9b9e9e5e130cde785a30f6445b48
omid-ak <omid7798@gmail.com>
2019-05-10 19:22:58 +0430

    connection to DB added

--- Predictorbot.py
+++ Predictorbot.py
@@ -1,14 +1,31 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-# import telegram
-# import requests
-# import pprint
+
+import Prediction
+import Home
+import Car
+
+
+############### Data Base Part #########################
+import mysql.connector
+
+dbconnector = mysql.connector.connect(host="127.0.0.1",
+                                      user="root",
+                                      password="@Omid1377",
+                                      )
+
+my_cursor = dbconnector.cursor()
+
+dbconnector.commit()
+dbconnector.close()
+
+
+
+
+
+############# Telegram Bot Part ##################################
 # Boturl = "https://api.telegram.org/bot634641815:AAFLp9QVqfPFbM32EFcoVpr4K70xk2JnzR0/getme"
-# response = requests.get(Boturl)
-# pprint.pprint(response.json())
 token = "634641815:AAFLp9QVqfPFbM32EFcoVpr4K70xk2JnzR0"
-# Bot = telegram.Bot(token=token)
-# print(Bot.get_me())
 
 #check for new message
 updater = Updater(token=token)
@@ -42,36 +59,12 @@ button_handler = CallbackQueryHandler(button)
 dispatcher.add_handler(button_handler)
 
 
-
-# def sendmsg(bot, update):
-#     bot.send_message(chat_id=update.message.chat_id, text="سلام دوست عزیز به بات پیش بینی قیمت خودرو و خانه خوش آمدید :)")
-#
-#
-# msg_handler = MessageHandler(Filters.text, sendmsg)
-# dispatcher.add_handler(msg_handler)
-
-
-
-#create a new command handleer
-
-#start_handler = CommandHandler("start", start)
-
-#add the commadnhandler to dispatcher
-
-#dispatcher.add_handler(start_handler)
-
 #car handler
 def car(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="پیش بینی قیمت خودرو")
 car_handler = CommandHandler("car", car)
 dispatcher.add_handler(car_handler)
 
-# def sendmsg(bot, update):
-#     bot.send_message(chat_id=update.message.chat_id, text="پیش بینی قیمت خودرو")
-#
-#
-# msg_handler = MessageHandler(Filters.text, sendmsg)
-# dispatcher.add_handler(msg_handler)
 
 
 
@@ -81,32 +74,6 @@ def home(bot, update):
 home_handler = CommandHandler("home", home)
 dispatcher.add_handler(home_handler)
 
-# def sendmsg(bot, update):
-#     bot.send_message(chat_id=update.message.chat_id, text="پیش بینی خونه")
-#
-#
-# msg_handler = MessageHandler(Filters.text, sendmsg)
-# dispatcher.add_handler(msg_handler)
-
-
-# def choose(bot, update):
-#     button = [
-#         [InlineKeyboardButton("car", callback_data="ماشین")],
-#         [InlineKeyboardButton("home", callback_data="خونه")]
-#     ]
-#     reply_markup = InlineKeyboardMarkup(button)
-#
-#     bot.send_message(chat_id=update.message.chat_id, text="قیمت کدومو برات پیش بینی کنم :) ؟؟", reply_markup=reply_markup)
-# choose_handler = CommandHandler("Choose", choose)
-# dispatcher.add_handler(choose_handler)
-#
-# def button(bot, update):
-#     query = update.callback_query
-#
-#     bot.send_message(chat_id=update.message.chat_id, text="خب ! یرم برای پیش بینی قیمت {}".format(query.data))
-#
-# button_handler = CallbackQueryHandler(button)
-# dispatcher.add_handler(button_handler)
 
 
 
