f18827b0626b06c0ad0077b07e87c1b6facb109c
omid-ak <omid7798@gmail.com>
2019-05-14 00:04:44 +0430

    catching data and insert theme into db

--- Car.py
+++ Car.py
@@ -1,17 +1,30 @@
 import mysql.connector
 import requests
 import bs4
+########### creating DB and table #########
+import DBConfig
+DBConfig.CAR_DB_creating()
+DBConfig.CAR_DB_table_creating()
 
 ############ Connecting to DB #############
-
 dbconnector = mysql.connector.connect(host="127.0.0.1",
                                       user="root",
                                       password="@Omid1377",
+                                      database="CAR"
                                       )
-
 my_cursor = dbconnector.cursor()
-create_db_q = "CREATE DATABASE CAR;"
-my_cursor.execute(create_db_q)
+########### Connecting to site ############
+for page in range(100):
+    url = "https://bama.ir/car/all-brands/all-models/all-trims?page=%d" % (page)
+    response = requests.get(url)
+    soup = bs4.BeautifulSoup(response.content, 'html.parser')
+    model   = soup.find('h2', attrs={"itemprop": "name"}).text.strip()
+    color   = soup.find('span', attrs={"id":"ex-color"}).text.strip()
+    karkard = soup.find('p',attrs={"class":"price hidden-xs"}).text.strip()
+    price = soup.find('span',attrs={"itemprop":"price"}).text.strip()
+########### insert to DB #################
+    my_cursor.execute('INSERT INTO car_info VALUES (\'%s\', \'%s\', \'%s\', \'%s\');' % (model, color, karkard, price))
 
 dbconnector.commit()
 dbconnector.close()
+my_cursor.close()
