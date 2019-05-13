5f2c3348359ac22440f27a30c4a26841dc679662
omid-ak <omid7798@gmail.com>
2019-05-14 00:06:40 +0430

    connection to db and TODOS...

--- Prediction.py
+++ Prediction.py
@@ -3,18 +3,42 @@ import requests
 import bs4
 
 
-import Home
-import Car
 
-############# Connecting to DB ########################
+############# car Prediction ########################
 
-dbconnector = mysql.connector.connect(host="127.0.0.1",
-                                      user="root",
-                                      password="@Omid1377",
-                                      )
+def car_predicting():
+    dbconnector_C = mysql.connector.connect(host="127.0.0.1",
+                                          user="root",
+                                          password="@Omid1377",
+                                          database="CAR"
+                                          )
 
-my_cursor = dbconnector.cursor()
+    my_cursor_C = dbconnector_C.cursor()
+
+    #TODO: predict cars
 
 
-dbconnector.commit()
-dbconnector.close()
+
+    dbconnector_C.commit()
+    dbconnector_C.close()
+    my_cursor_C.close()
+
+
+
+############ Home Prediction ########################
+def home_predicting():
+    dbconnector_H = mysql.connector.connect(host="127.0.0.1",
+                                          user="root",
+                                          password="@Omid1377",
+                                          database="HOME"
+                                          )
+
+    my_cursor_H = dbconnector_H.cursor()
+
+    #TODO: Predict Home
+
+
+
+    dbconnector_H.commit()
+    dbconnector_H.close()
+    my_cursor_H.close()
