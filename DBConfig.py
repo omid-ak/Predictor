d7659089fb5a0e824107fb4f26a5c1698a33870d
omid-ak <omid7798@gmail.com>
2019-05-19 04:48:53 +0430

    changes in columns

--- DBConfig.py
+++ DBConfig.py
@@ -17,16 +17,17 @@ def HOME_DB_table_creating():
     my_cursor.execute("USE HOME;")
     my_cursor.execute("CREATE TABLE IF NOT EXISTS home_info (ID INTEGER AUTO_INCREAMENT PRIMARY KEY ,"
                       "location VARCHAR(50),"
-                      "building_age VARCHAR(50), "
                       "rooms VARCHAR(50), "
-                      "meterinx VARCHAR(50), price VARCHAR(50));")
+                      "meterix VARCHAR(50), price VARCHAR(50)),"
+                      "price VARCHAR(50);")
 
 def CAR_DB_creating():
     my_cursor.execute("CREATE DATABASE CAR;")
 def CAR_DB_table_creating():
     my_cursor.execute("CREATE TABLE IF NOT EXISTS home_info (ID INTEGER AUTO_INCREAMENT PRIMARY KEY ,"
                       "model VARCHAR(50),"
-                      "clor VARCHAR(50), "
+                      "year_car VARCHAR (50)"
+                      "color VARCHAR(50), "
                       "karkard VARCHAR(50),"
                       "price VARCHAR(50));")
 dbconnector.commit()
