b23cb0686b048f34c8020079c0e0d69b24970341
omid-ak <omid7798@gmail.com>
2019-05-23 17:11:14 +0430

    id added to table

--- DBConfig.py
+++ DBConfig.py
@@ -15,7 +15,7 @@ def HOME_DB_creating():
 
 def HOME_DB_table_creating():
     my_cursor.execute("USE HOME;")
-    my_cursor.execute("CREATE TABLE IF NOT EXISTS home_info (ID INTEGER AUTO_INCREAMENT PRIMARY KEY ,"
+    my_cursor.execute("CREATE TABLE IF NOT EXISTS home_info (ID INTEGER AUTO_INCREMENT PRIMARY KEY ,"
                       "location VARCHAR(50),"
                       "rooms VARCHAR(50), "
                       "meterix VARCHAR(50), price VARCHAR(50)),"
@@ -24,7 +24,7 @@ def HOME_DB_table_creating():
 def CAR_DB_creating():
     my_cursor.execute("CREATE DATABASE CAR;")
 def CAR_DB_table_creating():
-    my_cursor.execute("CREATE TABLE IF NOT EXISTS home_info (ID INTEGER AUTO_INCREAMENT PRIMARY KEY ,"
+    my_cursor.execute("CREATE TABLE IF NOT EXISTS home_info (ID INTEGER AUTO_INCREMENT PRIMARY KEY ,"
                       "model VARCHAR(50),"
                       "year_car VARCHAR (50)"
                       "color VARCHAR(50), "
