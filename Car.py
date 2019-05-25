b4e1a95d7d6e31c00dcf036915bcf8ae9a9f8bb6
omid-ak <omid7798@gmail.com>
2019-05-26 02:08:50 +0430

    format to function

--- Car.py
+++ Car.py
@@ -1,42 +1,44 @@
 import mysql.connector
 import requests
 import bs4
-########### creating DB and table #########
 import DBConfig
-DBConfig.HOME_CAR_creating()
-DBConfig.CAR_table_creating()
+########### creating DB and table #########
 
-############ Connecting to DB #############
-dbconnector = mysql.connector.connect(host="127.0.0.1",
-                                      user="root",
-                                      password="@Omid1377",
-                                      database="H_C",
-                                      table="car_info"
-                                      )
-my_cursor = dbconnector.cursor()
-########### Connecting to site ############
-id = 0
-id2 = 0
-for page in range(291):
-    url = "https://takhtegaz.com/car/sale?page=%i" % page
-    response = requests.get(url)
-    soup = bs4.BeautifulSoup(response.content, 'html.parser')
-########### insert to DB #################
-    for m in  soup.find_all('h3', attrs={"class":"card-main__title"}):
-        temp = m.get_text().strip()
-        year = temp[0:5]
-        model = temp[5:len(temp)-11]
-        my_cursor.execute("INSERT INTO car_info(model,year_car) VALUES(\'%s\', \'%s\');" % (model, year))
-    for kc in soup.find_all('ul', attrs={"class":"specs hidden-md-down clearfix"}):
-        id += 1
-        templs = kc.get_text().strip().split()
-        karkard  = templs[0]
-        color = templs[3]
-        my_cursor.execute("UPDATE car_info SET karkard = \'%s\', color = \'%s\' WHERE ID = \'%d\';" % (karkard, color, id))
-    for p in soup.find_all('div', attrs={"class":"card__price money-format"}):
-        id2 += 1
-        price = p.get_text().strip(" تومان")
-        my_cursor.execute("UPDATE car_info SET prcie = \'%s\' WHERE ID = \'%d\'" % (price, id2))
-dbconnector.commit()
-dbconnector.close()
-my_cursor.close()
+def CAR_data_fetching():
+    DBConfig.HOME_CAR_creating()
+    DBConfig.CAR_table_creating()
+
+    ############ Connecting to DB #############
+    dbconnector = mysql.connector.connect(host="127.0.0.1",
+                                          user="root",
+                                          password="@Omid1377",
+                                          database="H_C",
+                                          table="car_info"
+                                          )
+    my_cursor = dbconnector.cursor()
+    ########### Connecting to site ############
+    id = 0
+    id2 = 0
+    for page in range(291):
+        url = "https://takhtegaz.com/car/sale?page=%i" % page
+        response = requests.get(url)
+        soup = bs4.BeautifulSoup(response.content, 'html.parser')
+    ########### insert to DB #################
+        for m in  soup.find_all('h3', attrs={"class":"card-main__title"}):
+            temp = m.get_text().strip()
+            year = temp[0:5]
+            model = temp[5:len(temp)-11]
+            my_cursor.execute("INSERT INTO car_info(model,year_car) VALUES(\'%s\', \'%s\');" % (model, year))
+        for kc in soup.find_all('ul', attrs={"class":"specs hidden-md-down clearfix"}):
+            id += 1
+            templs = kc.get_text().strip().split()
+            karkard  = templs[0]
+            color = templs[3]
+            my_cursor.execute("UPDATE car_info SET karkard = \'%s\', color = \'%s\' WHERE ID = \'%d\';" % (karkard, color, id))
+        for p in soup.find_all('div', attrs={"class":"card__price money-format"}):
+            id2 += 1
+            price = p.get_text().strip(" تومان")
+            my_cursor.execute("UPDATE car_info SET prcie = \'%s\' WHERE ID = \'%d\'" % (price, id2))
+    dbconnector.commit()
+    dbconnector.close()
+    my_cursor.close()
--- Home.py
+++ Home.py
@@ -1,41 +1,44 @@
 import mysql.connector
 import requests
 import bs4
-############ creating DB and table ###################
 import DBConfig
-DBConfig.HOME_CAR_creating()
-DBConfig.HOME_table_creating()
+############ creating DB and table ###################
 
-############### Connecting to DB #####################
+def Home_data_fetching():
+    DBConfig.HOME_CAR_creating()
+    DBConfig.HOME_table_creating()
 
-dbconnector = mysql.connector.connect(host="127.0.0.1",
-                                      user="root",
-                                      password="@Omid1377",
-                                      database="H_C",
-                                      table="home_info"
-                                      )
-my_cursor = dbconnector.cursor()
-############### Connecting to site ###################
-id = 0
-id2 = 0
-for page in range(50049):
-    url = "https://www.ihome.ir/%D8%AE%D8%B1%DB%8C%D8%AF-%D9%81%D8%B1%D9%88%D8%B4/%D8%A7%D9%85%D9%84%D8%A7%DA%A9/%D8%A7%DB%8C%D8%B1%D8%A7%D9%86/%d/" % page
-    response = requests.get(url)
-    soup = bs4.BeautifulSoup(response.content, 'html.parser')
-############### insert datas to DB ###################
-    for l in soup.find_all('div', attrs={'class':'location'}):
-        location = l.get_text().strip()
-        my_cursor.execute("INSERT INTO home_info(location) VALUES (\'%s\');" % location)
-    for rome in soup.find_all('ul', attrs={"class":"left slider_pinfo"}):
-        id += 1
-        templs = rome.get_text().strip().split()
-        rooms = rome[0]
-        meterix = rome[2]
-        my_cursor.execute("UPDATE home_info SET rooms = \'%s\', meterix = \'%s\' WHERE ID = \'%d\';" % (rooms, meterix, id))
-    for p in soup.find_all('div', attrs={'class':'price','data-cur-area':'Sq. M.'}):
-        id2 += 1
-        price = p.get_text().strip().strip('تومان')
-        my_cursor.execute("UPDATE home_info SET price = \'%s\' WHERE ID = \'%d\';" % (price, id2))
-dbconnector.commit()
-dbconnector.close()
-my_cursor.close()
+    ############### Connecting to DB #####################
+
+
+    dbconnector = mysql.connector.connect(host="127.0.0.1",
+                                          user="root",
+                                          password="@Omid1377",
+                                          database="H_C",
+                                          table="home_info"
+                                          )
+    my_cursor = dbconnector.cursor()
+    ############### Connecting to site ###################
+    id = 0
+    id2 = 0
+    for page in range(50049):
+        url = "https://www.ihome.ir/%D8%AE%D8%B1%DB%8C%D8%AF-%D9%81%D8%B1%D9%88%D8%B4/%D8%A7%D9%85%D9%84%D8%A7%DA%A9/%D8%A7%DB%8C%D8%B1%D8%A7%D9%86/%d/" % page
+        response = requests.get(url)
+        soup = bs4.BeautifulSoup(response.content, 'html.parser')
+    ############### insert datas to DB ###################
+        for l in soup.find_all('div', attrs={'class':'location'}):
+            location = l.get_text().strip()
+            my_cursor.execute("INSERT INTO home_info(location) VALUES (\'%s\');" % location)
+        for rome in soup.find_all('ul', attrs={"class":"left slider_pinfo"}):
+            id += 1
+            templs = rome.get_text().strip().split()
+            rooms = rome[0]
+            meterix = rome[2]
+            my_cursor.execute("UPDATE home_info SET rooms = \'%s\', meterix = \'%s\' WHERE ID = \'%d\';" % (rooms, meterix, id))
+        for p in soup.find_all('div', attrs={'class':'price','data-cur-area':'Sq. M.'}):
+            id2 += 1
+            price = p.get_text().strip().strip('تومان')
+            my_cursor.execute("UPDATE home_info SET price = \'%s\' WHERE ID = \'%d\';" % (price, id2))
+    dbconnector.commit()
+    dbconnector.close()
+    my_cursor.close()
